a7f6eaa1b2292d9d9016e5b1618eba5e8afcdbb0
Javier Gutierrez (Yav) <gutierrez.alcantara.javier@gmail.com>
2021-10-04 14:38:06 -0500

    Add styles bootstrap and CSS

--- src/index.js
+++ src/index.js
@@ -21,19 +21,41 @@ fetch(`${url}/api/avo`)
   responseJson.data.forEach(item => {
     // crear imagen
     const image = document.createElement('img')
+    image.className = 'img-fluid rounded-start'
     image.src = `${url}${item.image}`
     // crear titulo
     const title = document.createElement('h2')
+    title.className = 'card-title'
     title.textContent = `${item.name}`
     // crear precio
     const price = document.createElement('p')
+    price.className = 'card-text'
     price.textContent = formatPrice(item.price)
     
-    const container = document.createElement('div')
-    container.append(image, title, price)
-    container.className = ''
+    const divImage = document.createElement('div')
+    divImage.className = 'col-md-4'
+    divImage.appendChild(image)
+    
+    const divText = document.createElement('div')
+    divText.className = 'card-body'
+    divText.append(title, price)
 
-    todosLosItems.push(container)
+    const divBodyText = document.createElement('div')
+    divBodyText.className = 'col-md-8'
+    divBodyText.appendChild(divText)
+
+    const divContent = document.createElement('div')
+    divContent.className = 'row g-0'
+    divContent.append(divImage, divBodyText)
+    
+
+    const cardContainer = document.createElement('div')
+    cardContainer.className = 'card col'
+    cardContainer.style = 'max-width: 480px;'
+    
+    cardContainer.append(divContent)
+
+    todosLosItems.push(cardContainer)
 
   });
   appNode.append(...todosLosItems);
